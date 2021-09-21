27ea4cfca14f03ef420aa37b73de462504973853
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-22 04:55:51 +1000

    allow package to load on automerge

--- src/VTKDataIO.jl
+++ src/VTKDataIO.jl
@@ -9,19 +9,23 @@ using LightXML
 using IterTools
 using Printf
 
-const vtkns = pyimport_conda("vtk.util.numpy_support", "vtk")
-const vtk = pyimport_conda("vtk", "vtk")
-const np = pyimport_conda("numpy", "numpy")
+if get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") == "false"
+    @eval begin
+        const vtkns = pyimport_conda("vtk.util.numpy_support", "vtk")
+        const vtk = pyimport_conda("vtk", "vtk")
+        const np = pyimport_conda("numpy", "numpy")
 
-include("vtkreaders.jl")
-include("vtkwriters.jl")
-include("vtpwriter.jl")
-include("stlIO.jl")
-include("extra_readers.jl")
-include("PyVTK.jl")
-include("visualize.jl")
+        include("vtkreaders.jl")
+        include("vtkwriters.jl")
+        include("vtpwriter.jl")
+        include("stlIO.jl")
+        include("extra_readers.jl")
+        include("PyVTK.jl")
+        include("visualize.jl")
 
-export read_vtk, write_vtk, read_stl, write_stl, valid_to_write, PyVTK, 
-    _VTKDataTypes, visualize, write_x3d, write_ply, read_obj, read_ply, visualize_3ds
+        export read_vtk, write_vtk, read_stl, write_stl, valid_to_write, PyVTK, 
+            _VTKDataTypes, visualize, write_x3d, write_ply, read_obj, read_ply, visualize_3ds
+    end
+end
 
 end
